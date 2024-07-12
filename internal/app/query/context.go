fe504692ec7492847340c37ba205a66515ecc7b5
justjack1521 <henryt1521@hotmail.com>
2024-07-12 23:34:20 +1000

    little fix tehee

--- internal/app/query/context.go
+++ internal/app/query/context.go
@@ -3,13 +3,10 @@ package query
 import (
 	"context"
 	uuid "github.com/satori/go.uuid"
-	"mevhub/internal/domain/session"
 )
 
 type Context interface {
 	context.Context
 	UserID() uuid.UUID
 	PlayerID() uuid.UUID
-	Session() *session.Instance
-	SetSession(instance *session.Instance)
 }
