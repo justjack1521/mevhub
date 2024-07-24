8f9ced1d32b3b2843cc9619431fbe2caf01606d8
justjack1521 <henryt1521@hotmail.com>
2024-07-25 01:37:45 +1000

    added some logging

--- internal/app/server/game_server_host.go
+++ internal/app/server/game_server_host.go
@@ -68,9 +68,13 @@ func (c *GameServerHost) unregister(id uuid.UUID) {
 func (c *GameServerHost) action(request *GameActionRequest) {
 	if instance, exists := c.games[request.InstanceID]; exists {
 		instance.game.ActionChannel <- request.Action
+		c.logger.WithFields(logrus.Fields{
+			"instance.id": request.InstanceID,
+			"action.type": reflect.TypeOf(request.Action),
+		}).Info("game action received")
 	}
 	c.logger.WithFields(logrus.Fields{
 		"instance.id": request.InstanceID,
 		"action.type": reflect.TypeOf(request.Action),
-	}).Info("game action received")
+	}).Info("game action orphaned")
 }
