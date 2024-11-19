549ffe308f5df703329c0ff139d3aab40529d703
justjack1521 <henryt1521@hotmail.com>
2024-11-20 01:14:57 +1100

    key checking for session instance query

--- internal/adapter/memory/session_instance.go
+++ internal/adapter/memory/session_instance.go
@@ -3,12 +3,19 @@ package memory
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/go-redis/redis/v8"
 	uuid "github.com/satori/go.uuid"
 	"mevhub/internal/adapter/memory/dto"
 	"mevhub/internal/core/domain/session"
 	"strings"
 	"time"
+)
+
+var (
+	ErrSessionInstanceNotFoundByKey = func(key string) error {
+		return fmt.Errorf("session instance not found by key: %s", key)
+	}
 )
 
 const sessionKey = "lobby_session"
@@ -32,7 +39,18 @@ func (r *SessionInstanceRedisRepository) Exists(ctx context.Context, id uuid.UUI
 }
 
 func (r *SessionInstanceRedisRepository) QueryByID(ctx context.Context, id uuid.UUID) (*session.Instance, error) {
+
+	exists, err := r.Exists(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
 	var key = r.GenerateSessionKey(id)
+
+	if exists == false {
+		return nil, ErrSessionInstanceNotFoundByKey(key)
+	}
+
 	var cmd = r.client.HGetAll(ctx, key)
 	if cmd.Err() != nil {
 		return nil, cmd.Err()
