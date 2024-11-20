fbda6c3b646ff2c06bc9e63ee315d0366169a07b
justjack1521 <henryt1521@hotmail.com>
2024-11-20 14:41:38 +1100

    discarding invalid disconnects

--- internal/core/application/consumer/client_disconnect.go
+++ internal/core/application/consumer/client_disconnect.go
@@ -1,8 +1,10 @@
 package consumer
 
 import (
+	"fmt"
 	"github.com/justjack1521/mevium/pkg/mevent"
 	"github.com/justjack1521/mevrabbit"
+	uuid "github.com/satori/go.uuid"
 	"github.com/wagslane/go-rabbitmq"
 	"mevhub/internal/core/domain/player"
 	"time"
@@ -27,6 +29,10 @@ func NewClientDisconnectConsumer(publisher *mevent.Publisher, conn *rabbitmq.Con
 }
 
 func (s *ClientDisconnectConsumer) Consume(ctx *mevrabbit.ConsumerContext) (action rabbitmq.Action, err error) {
+	if ctx.UserID() == uuid.Nil || ctx.PlayerID() == uuid.Nil {
+		fmt.Println("Here's another")
+		return rabbitmq.NackDiscard, nil
+	}
 	var evt = player.NewDisconnectedEvent(ctx.Context, ctx.UserID(), ctx.PlayerID(), time.Now().UTC())
 	s.publisher.Notify(evt)
 	return rabbitmq.Ack, nil
